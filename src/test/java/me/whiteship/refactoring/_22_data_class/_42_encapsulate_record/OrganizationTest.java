34646c1ef619729f6c69d748661404f6648c20c3
EP <kjuioqq8@gmail.com>
2022-03-24 19:42:56 +0900

    냄새 22: 데이터 클래스

---
+++ src/test/java/me/whiteship/refactoring/_22_data_class/_42_encapsulate_record/OrganizationTest.java
@@ -0,0 +1,12 @@
+package me.whiteship.refactoring._22_data_class._42_encapsulate_record;
+
+import org.junit.jupiter.api.Test;
+
+class OrganizationTest {
+
+    @Test
+    void name() {
+
+    }
+
+}
